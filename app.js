f44af4196cce92a1e4009d99932bb33b87866017
Doug Walter <doug@kosmojo.com>
2015-02-04 18:30:10 -0700

    add bootstrap

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ app.controller('MainCtrl', [
 	'$scope',
 	function($scope){
 	  $scope.posts = [
-		{title: 'post 1', upvotes: 5},
-		{title: 'post 2', upvotes: 2},
-		{title: 'post 3', upvotes: 4},
-		{title: 'post 4', upvotes: 9}
+		{title: 'post 1', upvotes: 5, link: 'stuff.com'},
+		{title: 'post 2', upvotes: 2, link: 'stuff.com'},
+		{title: 'post 3', upvotes: 4, link: 'stuff.com'},
+		{title: 'post 4', upvotes: 9, link: 'stuff.com'}
 		];
 	  $scope.addPost = function (){
 	  	if(!$scope.title || $scope.title === '') { return; }
-	  	$scope.posts.push({title: $scope.title, upvotes: 0});
+	  	$scope.posts.push({
+	  		title: $scope.title, 
+	  		upvotes: 0, 
+	  		link: $scope.link});
+	  };
+	  $scope.incrementUpvotes = function(post){
+	  	post.upvotes += 1;
 	  };
 
 }]);
