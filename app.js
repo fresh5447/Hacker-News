4241e958b9e44b1ed3b4b90879d1103d4cab5632
Doug Walter <doug@kosmojo.com>
2015-02-04 19:35:49 -0700

    beginning node

--- app.js
+++ app.js
@@ -1,23 +1,68 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+ '$stateProvider',
+ '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      })
+      .state('posts', {
+      	url: '/posts{id}',
+      	templateUrl: 'posts.html',
+      	controller: 'PostsCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+  }]);
+
+app.factory('posts', [function () {
+	var o = {
+		posts: []
+	};
+
+	return o;
+}]);
+
+app.controller('PostsCtrl', [
+	'$scope',
+	'$scope.Params',
+	'$posts', 
+	function($scope, $stateParams, $posts){
+	  $scope.addComment = function (){
+	  	if($scope.body === '') { return; }
+	  	$scope.post.comments.push({
+	  		body: $scope.body,
+	  		author: 'user',
+	  		upvotes: 0
+	  	});
+	  	$scope.body = '';
+	  }
+}]);
 
 app.controller('MainCtrl', [
 	'$scope',
-	function($scope){
-	  $scope.posts = [
-		{title: 'post 1', upvotes: 5, link: 'stuff.com'},
-		{title: 'post 2', upvotes: 2, link: 'stuff.com'},
-		{title: 'post 3', upvotes: 4, link: 'stuff.com'},
-		{title: 'post 4', upvotes: 9, link: 'stuff.com'}
-		];
+	'posts',
+	function($scope, posts){
+	  $scope.post = posts.post[$stateParams.id]
+	  $scope.posts = posts.posts
 	  $scope.addPost = function (){
 	  	if(!$scope.title || $scope.title === '') { return; }
 	  	$scope.posts.push({
 	  		title: $scope.title, 
 	  		upvotes: 0, 
-	  		link: $scope.link});
+	  		link: $scope.link,
+	  		comments: [
+	  		  {author: 'Joe', body: 'Cool post!', upvotes: 0},
+	  		  {author: 'Bob', body: 'Lame post!', upvotes: 0}
+	  		]
+	  	});
 	  };
 	  $scope.incrementUpvotes = function(post){
 	  	post.upvotes += 1;
 	  };
-
 }]);
